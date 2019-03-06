cc8484a2d4991ce868925de3595d5f1afd732a59
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2019-03-06 18:07:35 +1100

    Final move before heading back to the other git directory

--- tests/testthat/test-extractor.R
+++ tests/testthat/test-extractor.R
@@ -70,7 +70,9 @@ testD <- c("g = 12.32",
            "d = 1",
            "d = 1232.23")
 
-testEta <- c()
+testEta <- c("η2 = .3213",
+             "eta squared = 1.232",
+             "η2 = .3213")
 
 
 testTString  <- str_flatten(testT, collapse = " ")
@@ -78,6 +80,7 @@ testFString  <- str_flatten(testF, collapse = " ")
 testRString  <- str_flatten(testR, collapse = " ")
 testChiString  <- str_flatten(testChi, collapse = " ")
 testDString <- str_flatten(testD, collapse = " ")
+testEtaString <- str_flatten(testEta, collapse = " ")
 
 
 test_that("t test extractor works", {
@@ -95,6 +98,7 @@ test_that("correlational test extractor works", {
   expect_identical(extractTestStats(testRString)[[2]],
                      str_remove_all(testR, "\\s")[-9])
 })
+
 test_that("chi test extractor works", {
   expect_identical(extractTestStats(testChiString)[[3]], testChi)
   expect_identical(extractTestStats(testChiString)[[2]],
@@ -102,10 +106,10 @@ test_that("chi test extractor works", {
 })
 
 
-test_that("", {
-  expect_identical(extractTestStats(testChiString)[[3]], testChi)
-  expect_identical(extractTestStats(testChiString)[[2]],
-                   str_remove_all(testChi, "\\s"))
+test_that("eta squared extractor works", {
+  expect_identical(extractTestStats(testEtaString)[[3]], testEta)
+  expect_identical(extractTestStats(testEtaString)[[2]],
+                   str_remove_all(testEta, "\\s"))
 })
 
 test_that("cohen's d extractor works", {
